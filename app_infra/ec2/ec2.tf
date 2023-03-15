f7b7f53cbf9bed06e7e891631620501e0565ce11
Ubuntu <ubuntu@ip-10-0-1-136.ec2.internal>
2023-03-15 20:26:19 +0000

    app-db

---
+++ app_infra/ec2/ec2.tf
@@ -0,0 +1,12 @@
+module "ec2_instance" {
+  source                      = "../../modules/ec2_module"
+  ami_id                      = "ami-0557a15b87f6559cf"
+  instance_type               = "t2.micro"
+  vpc_security_group_ids      = ["sg-0a6964789e2d5ac34"]
+  subnet_id                   = "subnet-088121577a178d9e2"
+  associate_public_ip_address = true
+  key_name                    = "iman"
+  tags = {
+    Name = "PLG"
+  }
+}
---
+++ app_infra/s3/s3.tf
@@ -0,0 +1,4 @@
+module "s3" {
+  source = "../../modules/s3_module"
+  bucket_name = "imans3"
+}
