7920cebde6576e23a4f93603075cf56e4fb70529
Ubuntu <ubuntu@ip-10-0-1-136.ec2.internal>
2023-03-21 21:59:32 +0000

    iman

--- app_infra/ec2/ec2.tf
+++ app_infra/ec2/ec2.tf
@@ -1,6 +1,6 @@
 module "ec2_instance" {
   source                      = "github.com/sanijoi/MODULES/modules/ec2_module"
-  ami_id                      = "ami-0557a15b87f6559cf"
+  ami_id                      = "ami-038da2997d5cba1f8"
   instance_type               = "t2.micro"
   vpc_security_group_ids      = ["sg-0a6964789e2d5ac34"]
   subnet_id                   = "subnet-088121577a178d9e2"
